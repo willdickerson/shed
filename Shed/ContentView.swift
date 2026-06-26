951116ee7e38b504f4109757bcbe8cc375102a7a
Will Dickerson <will.e.dickerson@gmail.com>
2026-06-26 07:56:34 -0500

    Initial Commit

---
+++ Shed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shed
+//
+//  Created by Will Dickerson IV on 6/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shed/ShedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShedApp.swift
+//  Shed
+//
+//  Created by Will Dickerson IV on 6/26/26.
+//
+
+import SwiftUI
+
+@main
+struct ShedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
